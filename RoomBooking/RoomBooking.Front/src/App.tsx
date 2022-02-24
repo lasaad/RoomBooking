d6dce974706599accd8c2588156df22f2e0803f8
Lasaad <lasaad.benzaied@gmail.com>
2022-02-24 15:52:04 +0100

    ajout user au front

--- RoomBooking/RoomBooking.Front/src/App.tsx
+++ RoomBooking/RoomBooking.Front/src/App.tsx
@@ -2,12 +2,14 @@ import { Provider } from "react-redux";
 import "./App.css";
 import { store } from "./app/store";
 import Rooms from "./components/Rooms";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <Rooms />
+        <Users />
       </Provider>
     </div>
   );
